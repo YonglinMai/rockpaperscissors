09e84c5edb020294827888304a81669e280f8792
YonglinMai <yonglinmai@gmail.com>
2019-11-06 23:21:34 +0000

    11/6/19

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 // *************************** YOUR CODE BELOW *******************************
+$("#shoot").click(function(){
+    var input = ("#input").val();
+    var choice = ["rock","paper","scissors"];
+   // let computerChoice =
+    $("#userChoice").html(input);
+
+    console.log(input)
+
+});
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 

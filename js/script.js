de3ff51c1d68fdcb27e202e39cbdb1c90b80f5f8
YonglinMai <yonglinmai@gmail.com>
2019-11-13 23:23:52 +0000

    11/13/19

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
 // *************************** YOUR CODE BELOW *******************************
 $("#shoot").click(function(){
-    var input = ("#input").val();
-    var choice = ["rock","paper","scissors"];
-   // let computerChoice =
-    $("#userChoice").html(input);
+    let pchoice= $("#input").val();
+    $("#userChoice").html(pchoice);
 
-    console.log(input)
+    function computerchoice(number){
+        let number = Math.random();
+        if(number > 0.6){
+            computerResult = "rock";
+        }
+        if(number < 0.6 && number > 0.3){
+            computerResult = "paper";
+        }
+        else{
+            computerResult = "scissors";
+        }
+    }
+
+    let x = computerchoice()
+    $("#computerChoice").html(x);
+    if (pchoice == "rock" && x == "scissors"){
+        $("#result").html("Player win!");
+    }
 
 });
 //******************TEST EARLY AND OFTEN USING console.log() ******************
